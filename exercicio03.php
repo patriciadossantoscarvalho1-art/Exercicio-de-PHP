51f87af2ed57ee72ff1516eb1dbf9bbcb9b4ba0f
Patricia <patriciadossantoscarvalho1@gmail.com>
2026-09-07 05:49:05 +0300

    Add files via upload
    Exercicios de PHP. 
    Codigo de contagem progressiva, fatorial e tabuada.

---
+++ exercicio03.php
@@ -0,0 +1,8 @@
+<?php
+$numero = 5;
+    $fatorial = 1;
+        for ($i = 1; $i<= $numero; $i++){
+            $fatorial = $fatorial * $i;
+        }
+            echo "O fatorial de $numero é: $fatorial";
+            ?>
---
+++ exercicio04.php
@@ -0,0 +1,7 @@
+<?php
+    $numero = 7;
+        for ($i = 1; $i<=10; $i ++){
+            $resultado = $numero * $i;
+                echo "$numero * $i = $resultado<br>";
+        }
+        ?>
